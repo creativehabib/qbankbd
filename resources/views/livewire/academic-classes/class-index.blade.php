bf5d87b540fdae1171bfabe04151429c62328601
creativehabib <shamolrahaman@gmail.com>
2026-04-17 00:58:27 +0600

    update

--- resources/views/livewire/academic-classes/class-index.blade.php
+++ resources/views/livewire/academic-classes/class-index.blade.php
@@ -1,10 +1,4 @@
 <div class="space-y-6">
-    <div>
-        <flux:heading size="xl">Academic Structure</flux:heading>
-        <flux:subheading>Manage Classes smoothly.</flux:subheading>
-        <flux:separator class="my-6" />
-    </div>
-
     <div class="grid gap-6">
         <section class="rounded-2xl border border-gray-200 bg-white shadow-sm dark:border-gray-700 dark:bg-gray-800">
             <div class="border-b border-gray-100 px-5 py-4 dark:border-gray-700">
