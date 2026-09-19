3fbb9cea9712a8f8fda81bab3eea9cee53d47d9a
creativehabib <shamolrahaman@gmail.com>
2026-09-19 09:03:16 +0600

    update

--- clean_scripts.php
+++
@@ -1,9 +0,0 @@
-<?php
-$file = 'resources/views/frontend/scripts.blade.php';
-$content = file_get_contents($file);
-
-// Remove the livewire:navigated line
-$content = preg_replace('/document\.addEventListener\(\'livewire:navigated\', initThemeToggle\);\n?/', '', $content);
-
-file_put_contents($file, $content);
-echo "Cleaned up scripts.blade.php\n";
--- fix_scripts2.php
+++
@@ -1,9 +0,0 @@
-<?php
-$file = 'resources/views/frontend/scripts.blade.php';
-$content = file_get_contents($file);
-
-// Remove the initThemeToggle block
-$content = preg_replace('/function initThemeToggle\(\) \{[\s\S]*?\}\n\s*window\.addEventListener\(\'DOMContentLoaded\', initThemeToggle\);\n*/', '', $content);
-
-file_put_contents($file, $content);
-echo "Removed duplicate theme JS from scripts.blade.php\n";
